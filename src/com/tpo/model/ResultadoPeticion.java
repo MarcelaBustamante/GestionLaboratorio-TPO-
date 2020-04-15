6add64e3fd175c6b37276ca525bc9cf4dfa69dab
Juan Cruz Alric <juan.alric@corp.smtsoftware.com>
2020-04-15 18:31:47 -0300

    Added the class for ResultadoPeticion

---
+++ src/com/tpo/model/ResultadoPeticion.java
@@ -0,0 +1,14 @@
+package com.tpo.model;
+
+public class ResultadoPeticion {
+	
+	private String resultado;
+	private Peticion peticion;
+	
+	public String getResultado() {
+		return resultado;
+	}
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+}
